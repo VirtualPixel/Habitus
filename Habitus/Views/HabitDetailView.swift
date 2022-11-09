73b91020e3ac88cf0f6cc8fa3b1b480f192a5e4e
Justin Wells <wellsmjustin@gmail.com>
2022-11-08 20:39:56 -0500

    Created HabitView ProgressBar

--- Habitus/Views/HabitDetailView.swift
+++ Habitus/Views/HabitDetailView.swift
@@ -17,41 +17,70 @@ struct HabitDetailView: View {
         GeometryReader { geo in
                 VStack {
                     Button("Add 10%") {
-                        habit.currentValue += Int((Double(habit.endGoal) * 0.01))
-                        //print("Current Value: \(habit.currentValue)")
-                        print("Percent complete: \(habit.percentComplete)")
-                        print(habit.progressBar)
-                        
-                        
+                        habit.currentValue += Int((Double(habit.endGoal) * 0.1))
                     }
                     ZStack {
                         Text(habit.icon)
                     }
                     .padding(.vertical, geo.size.height / 4)
                     
-                    ZStack(alignment: .bottom) {
-                        Circle()
-                            .trim(from: 0.0, to: 0.5)
-                            .frame(width: geo.size.width)
-                            .foregroundColor(.darkGray)
-                            .rotationEffect(Angle(degrees: -180))
+                    ZStack {
                         
-                        Circle()
-                            .trim(from: 0.002, to: 0.5)
-                            .stroke(Color(UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 0.3)), style: StrokeStyle(lineWidth: 10, dash: [25]))
-                            .rotationEffect(Angle(degrees: -180))
-                            .padding()
-                        
-                        Circle()
-                            .trim(from: 0, to: habit.progressBar)
-                            .stroke(habit.color, lineWidth: 12.0)
-                            .frame(width: geo.size.width)
-                            .rotationEffect(Angle(degrees: -180))
+                        ZStack {
+                            Circle()
+                                .trim(from: 0.0, to: 0.5)
+                                .frame(width: geo.size.width)
+                                .foregroundColor(.darkGray)
+                                .rotationEffect(Angle(degrees: -180))
+                            
+                            Circle()
+                                .trim(from: 0.002, to: 0.5)
+                                .stroke(Color(UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 0.3)), style: StrokeStyle(lineWidth: 10, dash: [25]))
+                                .rotationEffect(Angle(degrees: -180))
+                                .padding()
+                            
+                            Circle()
+                                .trim(from: 0, to: habit.progressBar)
+                                .stroke(habit.color.opacity(2.0), style: StrokeStyle(lineWidth: 12.0, lineCap: .round))
+                                .frame(width: geo.size.width)
+                                .rotationEffect(Angle(degrees: -180))
+                            
+                            Rectangle()
+                                .foregroundColor(.darkGray)
+                                .offset(y: 195)
+                            
+                            Text("\(habit.currentValue)")
+                                .foregroundColor(.white)
+                                .offset(y: -100)
+                                .font(.custom("DIN Alternate Bold",
+                                              size: 34,
+                                              relativeTo: .largeTitle))
+                            
+                            Text("🏆 Goal: \(habit.endGoal)")
+                                .foregroundColor(.white)
+                                .offset(y: -50)
+                                .font(.custom("DIN Alternate Bold",
+                                              size: 23,
+                                              relativeTo: .largeTitle))
+                            
+                            
+                            if habit.percentComplete == 100 {
+                                Text("You did it! Great job.")
+                                    .foregroundColor(.white)
+                                    .font(.custom("DIN Alternate Bold",
+                                                  size: 23,
+                                                  relativeTo: .largeTitle))
+                            } else {
+                                Text("\(habit.percentComplete > 59 ? "You're almost there!" : "You've got this. Don't give up!")")
+                                    .foregroundColor(.white)
+                                    .font(.custom("DIN Alternate Bold",
+                                                  size: 23,
+                                                  relativeTo: .largeTitle))
+                            }
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                        .fixedSize()
                     }
-                    .fixedSize()
-                    .border(.yellow)
-                    .edgesIgnoringSafeArea(.all)
-                        
                 }
                 
         }
